d056c693f1647ab807d9868b705e49d01e2b46d0
JerryZhang <chinajiezhang@gmail.com>
2012-12-26 12:51:17 +0800

    Revert "keepsimple"
    This reverts commit fb2a1e5d96908747ec8af44c9e92f5440aa3ff2a.

--- footer.php
+++
@@ -1,8 +0,0 @@
-<div id="footer">
-	<p>Copyright&#169;2012 <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a><br />
-	Design By <a href="http://www.keepsimply.org">Jerry Zhang</a> | Powered by <a href="http://wordpress.org">WordPress</a></p>
-</div>
-</div>
-<?php wp_footer(); ?>
-</body>
-</html>
