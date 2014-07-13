93399ed0f55fe8abff9a8843a9577646b2a90315
JerryZhang <chinajiezhang@gmail.com>
2014-07-13 11:14:06 +0000

    update

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
-	<p>Copyright&#169;2011~2013 <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a><br />
+	<p>Copyright&#169;2014 <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a><br />
 	Design By <a href="http://www.perfect-is-shit.com/">Jerry Zhang</a> | Powered by <a href="http://wordpress.org">WordPress</a>
 </div>
 </div>
